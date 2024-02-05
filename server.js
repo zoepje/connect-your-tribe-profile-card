1bf4e99358ddaf0d3e677ec3c6fa4ef7f2d07400
zoepje <zoe.penner@hva.nl>
2024-02-05 12:53:18 +0100

    edit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import fetchJson from './helpers/fetch-json.js'
 
 // Haal data op uit de FDND API, ga pas verder als de data gedownload is
 const data = await fetchJson('https://fdnd.directus.app/items/person/3')
-// console.log(data); // uncomment om de opgehaalde data te checken
+//console.log(data); // uncomment om de opgehaalde data te checken
 
 // Maak een nieuwe express app aan
 const app = express()
