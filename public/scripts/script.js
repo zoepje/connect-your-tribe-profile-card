0a683177b3fd327b301fab0d15058279ad4a3a21
zoepje <zoe.penner@hva.nl>
2024-02-07 12:34:49 +0100

    kleine redesign

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,9 +1,6 @@
 const button = document.querySelector("#flip1");
 const button1 = document.querySelector("#flip2");
 
-var back = document.querySelector("#beneden");
-var front = document.querySelector("#boven");
-
 button.addEventListener("click", flip);
 button1.addEventListener("click", flip);
 
